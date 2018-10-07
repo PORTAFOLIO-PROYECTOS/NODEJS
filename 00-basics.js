7430bb2c2ca159b1c33164517a46ac235ab687f9
Hugo <hugo.rock20@hotmail.com>
2018-10-07 00:16:16 -0500

    00-basics.js

---
+++ 00-basics.js
@@ -0,0 +1,27 @@
+/*
+
+Organización de código JS
+    librerias/modulos
+    constantes
+    objectos/variables
+    funciones
+    eventos
+    ejecuciones
+
+Usar camelCase
+    Cuando una insturcción tenga una sola palabra, va en minpusculas p.e. require()
+    Solo las clases rompen esta regla, siempre va en mayúscula la letra inicial
+    EventeEmmiter()
+    Cuando una instrucción tenga 2 o mpas palabras, apartir de la segunda la primera letra va en mayúsculas createServer()
+
+    Tipos de CamelCase
+        UppperCalemCase
+            Date()
+            EenteEmmiter()
+        lowerCalemCase
+            getElementById()
+            createServer()
+
+*/
+
+'use stict'; //modo estricto para programas con buenas prácticas
--- promise.js
+++ promise.js
@@ -2,10 +2,16 @@
 
 const fs = require('fs');
 
-let promise = new Promise((resolve, reject) => {
+/*let promise = new Promise((resolve, reject) => {
     fs.readFile('archivo.txt', (error, content) => {
         return (error) ? reject(new Error('E archivo no se pudo leer')) : resolve(content);
     });
+});*/
+// cualquiera de los funciona igual ↓ ↑
+let promise = new Promise(function (resolve, reject){
+    fs.readFile('archivo.txt', function(error, content){
+        return error ? reject(new Error('error, no se pueod abrir el archivo')) : resolve(content);
+    })
 });
 
 console.log('\nAbriendo Archivo...');
