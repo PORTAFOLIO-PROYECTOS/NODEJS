0b863f6055bba5445b9c75204cdf2be24d48c079
Hugo <hugo.rock20@hotmail.com>
2018-10-10 23:55:12 -0500

    12-event-emitter.js

---
+++ 12-event-emitter.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const EventEmitter = require('events').EventEmitter,
+    ee = new EventEmitter();
+
+ee
+    .on('myevent', message => console.log(message))
+    .once('myevent', message => console.log(`Se emite la primera vez: ${message}`));
+
+ee.emit('myevent', 'soy un emisor de eventos');
+ee.emit('myevent', 'volviendo a emitir');
