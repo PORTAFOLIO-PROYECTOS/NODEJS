0321b4e1e9aed9181d8c879421e7c90d30dbf82c
Hugo <hugo.rock20@hotmail.com>
2018-10-11 23:59:33 -0500

    15-modules adavence

---
+++ 15-modules.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * Módulo (require/exports)
+ *      require(<paquete o ruta>)
+ *          Importar módulos (paquetes, otros ficheros)
+ *          Garantía: una única vez
+ *          Devuelve el módulo
+ * 
+ *      exports.propiedaPublica = <valor>
+ *          El otro lado del mecanismo
+ *          Se pude exportar cualquier valor
+ * 
+ */
