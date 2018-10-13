ea9b359b8374f35afa51f1911ae3f294f2f5f126
Hugo <hugo.rock20@hotmail.com>
2018-10-12 23:35:48 -0500

    15-modules.js

--- 15-modules.js
+++ 15-modules.js
@@ -11,3 +11,13 @@
  *          Se pude exportar cualquier valor
  * 
  */
+
+ 'use strict';
+
+ const Clock = require('./Clock'),
+    myData = require('./my-data'),
+    cucu = new Clock();
+
+console.log(myData.name, myData.email, myData._phone);
+
+cucu.theTime();
---
+++ my-data.js
@@ -0,0 +1,11 @@
+'use strict';
+
+//para recursos privados anteponer el "_"
+
+let name = 'Hugo',
+    email = 'hugo.rock20@hotmail.com',
+    _phone = '945978987';
+
+module.exports.name = name;
+module.exports.email = email;
+module.exports._phone = _phone;
