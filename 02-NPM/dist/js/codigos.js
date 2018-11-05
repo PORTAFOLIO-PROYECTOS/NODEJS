d06b02bcc9f571bb73e85cb3105953ce8d9305a0
Hugo <hugo.rock20@hotmail.com>
2018-11-04 23:03:12 -0500

    función autoejecutable más funcionalidad de enlaces

--- 02-NPM/dist/js/codigos.js
+++ 02-NPM/dist/js/codigos.js
@@ -1,6 +1,28 @@
 'use strict';
 
-$(function () {
-    var nombre = 'Hugo';
-    //alert(`Hola ${nombre}`);
-});
+// Funciones autoejecutables
+
+(function (d, w, $, WOW) {
+
+    'use strict';
+
+    var wow = new WOW();
+
+    $(d).ready(function () {
+        $('.owl-carousel').owlCarousel({
+            items: 3,
+            loop: true,
+            autoplay: true,
+            autoplayTimeout: 1000,
+            autoplayHoverPause: true
+        });
+
+        $('.nav').find('a').click(function (e) {
+            var idEnlace = $(this).attr('href');
+
+            $('html, body').animate({
+                scrollTop: $(idEnlace).offset().top
+            }), 1000;
+        });
+    });
+})(document, window, jQuery, WOW);
--- 02-NPM/src/es6/codigos.js
+++ 02-NPM/src/es6/codigos.js
@@ -1,6 +1,27 @@
-'use strict';
+// Funciones autoejecutables
 
-$(() => {
-    let nombre = 'Hugo';
-    //alert(`Hola ${nombre}`);
-})
+((d, w, $, WOW) => {
+    
+    'use strict';
+
+    const wow = new WOW();
+
+    $(d).ready(() => {
+        $('.owl-carousel').owlCarousel({
+            items: 3,
+            loop: true,
+            autoplay: true,
+            autoplayTimeout: 1000,
+            autoplayHoverPause: true
+        });
+
+        $('.nav').find('a').click(function (e){
+            let idEnlace = $(this).attr('href');
+
+            $('html, body').animate({
+                scrollTop: $(idEnlace).offset().top
+            }), 1000;
+        });
+    });
+
+})(document, window, jQuery, WOW);
